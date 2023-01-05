de20bc0b1a9d3d599c4bd2a42e47d54473474b57
XveniusX <gokbaran2003.01@gmail.com>
2023-01-05 12:53:38 +0300

    pygameyi py olarak kullanıma açtım oyun görsellerini yükleyip çalıştırmayı deniyorum

--- başlangıç.py
+++ başlangıç.py
@@ -1,3 +1,13 @@
 #OYUNN YAZILIMINA BAŞLANIYOR NE TARZ OYUN OLDUĞUNU SEÇ
-import pygame 
+#kullanılacak modüller eklendi
+import pygame  as py
 import os
+import random
+#açılış penceresi yazılıyor
+genişlik= 750
+yükseklik= 600
+ekran=py.display.set_mode((genişlik,yükseklik))
+
+#resimleri yüklemek
+
+BG=py.image.load(os.path.join())

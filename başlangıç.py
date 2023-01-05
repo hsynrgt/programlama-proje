d25b8a28f1edbe01d3b3a7a414bb3069794adaba
XveniusX <gokbaran2003.01@gmail.com>
2023-01-05 13:08:38 +0300

    DEĞİŞİKLİKLER ve açılış penceresi yazılımı

--- başlangıç.py
+++ başlangıç.py
@@ -1,13 +1,39 @@
 #OYUNN YAZILIMINA BAŞLANIYOR NE TARZ OYUN OLDUĞUNU SEÇ
 #kullanılacak modüller eklendi
+
 import pygame  as py
 import os
 import random
-#açılış penceresi yazılıyor
+
+#penceresi yazılıyor
+
 genişlik= 750
 yükseklik= 600
 ekran=py.display.set_mode((genişlik,yükseklik))
 
 #resimleri yüklemek
 
-BG=py.image.load(os.path.join())
+BG=py.image.load(os.path.join("proje için","background_space.png"))
+
+
+
+def main():
+    run = True
+
+
+
+
+    def çizmek():
+        ekran.blit(BG, (0,0))
+
+        py.display.update()
+
+
+    while run:
+        çizmek()
+
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                run =False
+
+main()
